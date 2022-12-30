c442770773d8174a8f4c7cefa48e2dec3c387d51
ligalaiz <slavakuz88@gmail.com>
2022-12-30 22:52:11 +0300

    feat: implement event hadlers

--- src/pages/notab/notab.js
+++ src/pages/notab/notab.js
@@ -1,1 +1,18 @@
 import '@src/assets/styles/global/index.scss';
+
+const handleClick = (e) => {
+  const cookies = document.getElementById('cookies');
+
+  if (e.target?.id === 'cookiesBtn') cookies.classList.remove('active');
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
+
+const handleLoad = () => {
+  document.addEventListener('click', handleClick);
+  document.addEventListener('submit', handleSubmit);
+};
+
+window.addEventListener('load', handleLoad);
